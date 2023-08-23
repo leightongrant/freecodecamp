0279cbd7aaf2f6cc4766640db2e3e9a533bb11dc
Leighton Grant <oneal.grant@outlook.com>
2023-08-23 18:27:09 +0100

    Adds components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
 import RandomQuoteMachine from './pages/RandomQuoteMachine';
 
