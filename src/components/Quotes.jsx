7f02ad6d7fd47124661aaec2abaa0aef00835e9b
Leighton Grant <oneal.grant@outlook.com>
2023-08-25 22:28:26 +0100

    Adds quotes component

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,5 +1,21 @@
-function Quotes() {
-  return <>Random Quote Machine</>;
+/* eslint-disable react/prop-types */
+import Button from 'react-bootstrap/Button';
+import { FaInstagram, FaTwitter } from 'react-icons/fa';
+function Quotes({ quote, color }) {
+  const { text, author } = quote;
+  return (
+    <div id='quote-box' style={{ color: color }}>
+      <p id='text'>{text}</p>
+      <p id='author'>{author}</p>
+      <Button style={{ backgroundColor: color, border: 0 }}>
+        <FaTwitter />
+      </Button>
+      <Button style={{ backgroundColor: color, border: 0 }}>
+        <FaInstagram />
+      </Button>
+      <Button style={{ backgroundColor: color, border: 0 }}>New Quote</Button>
+    </div>
+  );
 }
 
 export default Quotes;
--- src/pages/RandomQuoteMachine.jsx
+++ src/pages/RandomQuoteMachine.jsx
@@ -1,9 +1,15 @@
 import Quotes from '../components/Quotes';
+import { useState } from 'react';
 
 function RandomQuoteMachine() {
+  const [quote, setQuote] = useState({
+    text: 'Your time is limited, so don’t waste it living someone else’s life.',
+    author: 'Steve Jobs',
+  });
+  const [color, setColor] = useState('navy');
   return (
-    <section>
-      <Quotes />
+    <section id='quote-box-wrapper' style={{ backgroundColor: color }}>
+      <Quotes quote={quote} setQuote={setQuote} color={color} />
     </section>
   );
 }
