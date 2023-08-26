5cc22383b6410e1f41f426a8fb8e2c9f5d3061bd
leightongrant <oneal.grant@outlook.com>
2023-08-26 15:58:23 +0100

    Adds styling

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,13 +1,21 @@
 /* eslint-disable react/prop-types */
 import Button from 'react-bootstrap/Button';
-import { FaInstagram, FaTwitter } from 'react-icons/fa';
-function Quotes({ quote, color }) {
-    const { text, author } = quote;
+import { FaInstagram, FaQuoteLeft, FaTwitter } from 'react-icons/fa';
+function Quotes({ quote, color, setColor, getColor }) {
+    const { content, author } = quote;
+    function handleClick() {
+        setColor(getColor());
+    }
+    function handleTweet() {
+        console.log({ content });
+    }
+
     return (
         <div id='quote-box' style={{ color: color }}>
             <figure className='mb-5'>
                 <blockquote className='blockquote text-center'>
-                    <p id='text'>{text}</p>
+                    <FaQuoteLeft />
+                    <p id='text'>{content}</p>
                 </blockquote>
                 <figcaption className='blockquote-footer d-flex justify-content-end'>
                     <span id='author'>{author}</span>
@@ -15,19 +23,21 @@ function Quotes({ quote, color }) {
             </figure>
             <div className='d-flex gap-3'>
                 <Button
-                    style={{ backgroundColor: color, border: 0 }}
+                    style={{ background: color, border: 0 }}
                     id='tweet-quote'
                     as='a'
+                    onClick={handleTweet}
                 >
                     <FaTwitter />
                 </Button>
-                <Button style={{ backgroundColor: color, border: 0 }}>
+                <Button style={{ background: color, border: 0 }}>
                     <FaInstagram />
                 </Button>
                 <Button
-                    style={{ backgroundColor: color, border: 0 }}
+                    style={{ background: color, border: 0 }}
                     id='new-quote'
                     className='ms-auto'
+                    onClick={handleClick}
                 >
                     New Quote
                 </Button>
--- src/pages/RandomQuoteMachine.jsx
+++ src/pages/RandomQuoteMachine.jsx
@@ -1,15 +1,40 @@
 import Quotes from '../components/Quotes';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function RandomQuoteMachine() {
     const [quote, setQuote] = useState({
-        text: 'Your time is limited, so don’t waste it living someone else’s life.',
-        author: 'Steve Jobs',
+        _id: '1WZnUSS47PTK',
+        content:
+            'We are either progressing or retrograding all the while. There is no such thing as remaining stationary in this life.',
+        author: 'James Freeman Clarke',
+        tags: ['Famous Quotes'],
+        authorSlug: 'james-freeman-clarke',
+        length: 117,
+        dateAdded: '2021-04-15',
+        dateModified: '2023-04-14',
     });
-    const [color, setColor] = useState('navy');
+    const [color, setColor] = useState('rgb(255,255,255)');
+
+    function getColor() {
+        const r = Math.random() * 200;
+        const g = Math.random() * 200;
+        const b = Math.random() * 200;
+        return `rgb(${r},${g},${b})`;
+    }
+
+    useEffect(() => {
+        //setColor(getColor());
+    }, []);
+
     return (
         <section id='quote-box-wrapper' style={{ background: color }}>
-            <Quotes quote={quote} setQuote={setQuote} color={color} />
+            <Quotes
+                quote={quote}
+                setQuote={setQuote}
+                color={color}
+                setColor={setColor}
+                getColor={getColor}
+            />
         </section>
     );
 }
