eb59a7a0485f26690f94b97ddecbb1f30c3144e4
Leighton Grant <oneal.grant@outlook.com>
2023-08-30 23:12:36 +0100

    Adds logic for calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,59 +1,122 @@
+/* eslint-disable react/prop-types */
 import Container from 'react-bootstrap/Container';
-function Calculator() {
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+function Calculator({ output, expressions }) {
+  const [display, setDisplay] = output;
+  const [expression, setExpression] = expressions;
+
+  function handleNum(e) {
+    const val = e.target.value;
+    if (/\./.test(expression) && val === '.') {
+      return;
+    }
+    if (expression === 0) {
+      setExpression(val);
+      setDisplay(val);
+    } else if (expression === '.') {
+      setDisplay(`0.${val}`);
+    } else if (/\d/.test(expression[expression.length - 1])) {
+      setExpression(preVal => (preVal += val));
+      setDisplay(preVal => (preVal += val));
+    } else {
+      setExpression(val);
+      setDisplay(preVal => (preVal += val));
+    }
+  }
+  function handleOperator(e) {
+    const val = e.target.value;
+    if (/\d/.test(expression[expression.length - 1])) {
+      setExpression(val);
+      setDisplay(pre => (pre += val));
+    }
+  }
+  function handleEquals(e) {
+    let val = e.target.value;
+
+    if (!/=/.test(display)) {
+      if (/^\d+$/.test(display) || expression === 0) {
+        console.log(display, 'else block');
+        if (display === null) {
+          setDisplay(expression);
+        } else {
+          val += expression;
+          setDisplay(pre => (pre += val));
+        }
+      } else if (/^\d+.+\d$/.test(display)) {
+        let answer = eval(display);
+        if (!Number.isInteger(answer)) {
+          answer = answer.toFixed(2);
+        }
+        val += String(answer);
+        setDisplay(pre => (pre += val));
+        setExpression(answer);
+      } else setDisplay('Invalid Expression');
+    }
+  }
+
+  function handleClear() {
+    setDisplay(null);
+    setExpression(0);
+  }
+
   return (
     <Container className='d-flex align-items-center justify-content-center'>
-      <div id='calc'>
-        <div id='display'>DISPLAY</div>
-        <div id='clear'>AC</div>
-        <div id='devide' className='operator'>
+      <Row id='calc'>
+        <output id='display'>{display}</output>
+        <output id='expression'>{expression}</output>
+        <Button className='btn btn-danger' id='clear' onClick={handleClear}>
+          AC
+        </Button>
+        <Button id='devide' value='/' className='operator btn btn-info' onClick={handleOperator}>
           /
-        </div>
-        <div id='multiply' className='operator'>
+        </Button>
+        <Button id='multiply' value='*' className='operator btn btn-info' onClick={handleOperator}>
           X
-        </div>
-        <div id='seven' className='num'>
+        </Button>
+        <Button id='seven' value='7' className='num btn btn-secondary' onClick={handleNum}>
           7
-        </div>
-        <div id='eight' className='num'>
+        </Button>
+        <Button id='eight' value='8' className='num btn btn-secondary' onClick={handleNum}>
           8
-        </div>
-        <div id='nine' className='num'>
+        </Button>
+        <Button id='nine' value='9' className='num btn btn-secondary' onClick={handleNum}>
           9
-        </div>
-        <div id='subtract' className='operator'>
+        </Button>
+        <Button id='subtract' value='-' className='operator btn btn-info' onClick={handleOperator}>
           -
-        </div>
-        <div id='four' className='num'>
+        </Button>
+        <Button id='four' value='4' className='num btn btn-secondary' onClick={handleNum}>
           4
-        </div>
-        <div id='five' className='num'>
+        </Button>
+        <Button id='five' value='5' className='num btn btn-secondary' onClick={handleNum}>
           5
-        </div>
-        <div id='six' className='num'>
+        </Button>
+        <Button id='six' value='6' className='num btn btn-secondary' onClick={handleNum}>
           6
-        </div>
-        <div id='add' className='operator'>
+        </Button>
+        <Button id='add' value='+' className='operator btn btn-info' onClick={handleOperator}>
           +
-        </div>
-        <div id='one' className='num'>
+        </Button>
+        <Button id='one' value='1' className='num btn btn-secondary' onClick={handleNum}>
           1
-        </div>
-        <div id='two' className='num'>
+        </Button>
+        <Button id='two' value='2' className='num btn btn-secondary' onClick={handleNum}>
           2
-        </div>
-        <div id='three' className='num'>
+        </Button>
+        <Button id='three' value='3' className='num btn btn-secondary' onClick={handleNum}>
           3
-        </div>
-        <div id='equals' className='operator'>
+        </Button>
+        <Button id='equals' value='=' className='operator btn btn-success' onClick={handleEquals}>
           =
-        </div>
-        <div id='zero' className='num'>
+        </Button>
+        <Button id='zero' value='0' className='num btn btn-secondary' onClick={handleNum}>
           0
-        </div>
-        <div id='decimal' className='num'>
+        </Button>
+        <Button id='decimal' value='.' className='num btn btn-secondary' onClick={handleNum}>
           .
-        </div>
-      </div>
+        </Button>
+      </Row>
     </Container>
   );
 }
--- src/pages/JavaScriptCalculator.jsx
+++ src/pages/JavaScriptCalculator.jsx
@@ -1,9 +1,12 @@
 import Calculator from '../components/Calculator';
+import { useState } from 'react';
 
 function JavaScriptCalculator() {
+  const [display, setDisplay] = useState(null);
+  const [expression, setExpression] = useState(0);
   return (
     <section id='js-calc' className='d-flex align-items-center justify-content-center'>
-      <Calculator />
+      <Calculator output={[display, setDisplay]} expressions={[expression, setExpression]} />
     </section>
   );
 }
